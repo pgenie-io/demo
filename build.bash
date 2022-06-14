156f393cc18e34902edbbbb831152d21a0259327
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-14 22:07:15 +0300

    Update to pgn

--- build.bash
+++ build.bash
@@ -4,12 +4,12 @@ set -eo pipefail
 function regenerate {
   rm -rf output/java-jdbc/src
   rm -rf output/haskell-hasql/library
-  pgenie
+  pgn-dev
 }
 
 function build_haskell_hasql {
   cd output/haskell-hasql
-  echo "resolver: nightly-2022-04-14" > stack.yaml
+  echo "resolver: nightly-2022-06-08" > stack.yaml
   stack build
   cd -
 }
